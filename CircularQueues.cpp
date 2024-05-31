2279d6a55ca76caf02b349d8838f0dec2a3a2090
LENOVO <MeutyaAzzahrani>
2024-05-31 15:24:02 +0700

    membuat switch menu

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -88,5 +88,26 @@ public:
 int main() {
     Queues q;
     char ch;
-    
-};
+
+    while (true) {
+        try {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choicr (1-4): ";
+            cin >> ch;
+            cout << endl;
+
+            
+                }
+                }
+            }
+            catch (exception& e) {
+                cout << "Check for the values entered." << endl;
+            }
+        }
+
+        return 0;
+    }
